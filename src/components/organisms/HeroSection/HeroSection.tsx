298841046d17ba0ee23bcb0ef75e4d5ea0562984
Yevhen Mosiichuk <zhenya9801@gmail.com>
2021-04-24 22:38:52 +0300

    Added 2 more sections to Home page

--- src/components/organisms/HeroSection/HeroSection.tsx
+++ src/components/organisms/HeroSection/HeroSection.tsx
@@ -76,7 +76,7 @@ const HeroSection: FC<HeroSectionProps> = ({
             </Container>
 
             <Hidden smDown>
-                <ShapeDivider bottom height={130} widthPercent={100}>
+                <ShapeDivider bottom height={130}>
                     <svg viewBox="0 0 1200 120" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
                         <path viewBox="0 0 1200 120" d="M0 73.2654C20.448 83.8096 80.4987 97.386 256.434 80.9949C516.959 56.7231 726.149 -77.66 1051.48 63.827C1288.37 166.846 1436.8 162.529 1500 152.566V170.608H0V73.2654Z"/>
                     </svg>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Box, Typography, Grid, Container, makeStyles} from "@material-ui/core";
+import {Box, Container, Grid, Hidden, makeStyles, Typography} from "@material-ui/core";
 import Image from "next/image";
 import HeroSection from "components/organisms/HeroSection/HeroSection";
 import SectionHeader from "components/atoms/SectionHeader/SectionHeader";
 import Button from "components/atoms/Button/Button";
+import ShapeDivider from "components/atoms/ShapeDivider/ShapeDivider";
 
 const useStyles = makeStyles((theme) => ({
     smFirstMdSecond: {
@@ -19,7 +20,16 @@ const useStyles = makeStyles((theme) => ({
         [theme.breakpoints.up('md')]: {
             order: 1,
         },
-    }
+    },
+    transferSection: {
+        background: theme.palette.primary.gradient,
+        padding: '242px 0 176px',
+    },
+    mobile: {
+        background: theme.palette.primary.gradient,
+        // padding: '155px 0 376px',
+        height: 531,
+    },
 }));
 
 const leftSlot = <>
@@ -90,6 +100,32 @@ const Homepage = () => {
                 </Container>
             </Box>
 
+            <Box position='relative' className={classes.transferSection}>
+                <Hidden smDown>
+                    <ShapeDivider top height={205}>
+                        <svg viewBox="0 0 1501 206" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio='none'>
+                            <path
+                                d="M0 95.1156C40.475 88.5219 109.051 86.6992 225.202 100.012C501.791 131.713 723.879 307.231 1069.28 122.435C1275.16 12.2796 1418.11 -3.91506 1500 0.973618V0H0"/>
+                        </svg>
+                    </ShapeDivider>
+                </Hidden>
+
+                {/*<Container>*/}
+
+                {/*</Container>*/}
+
+                <Hidden smDown>
+                    <ShapeDivider bottom height={121}>
+                        <svg width="1500" height="122" viewBox="0 0 1500 122" fill="none"
+                             xmlns="http://www.w3.org/2000/svg" preserveAspectRatio='none'>
+                            <path
+                                d="M432.981 49.0763C778.377 -59.7125 1000.47 43.6141 1277.05 62.2766C1391.07 69.97 1459.25 69.0771 1500 65.3698V122H0V120.497C81.5066 123.63 225.191 114.524 432.981 49.0763Z"
+                                fill="white"/>
+                        </svg>
+                    </ShapeDivider>
+                </Hidden>
+            </Box>
+
             <Box pt={12} pb={15}>
                 <Container>
                     <Grid container justify='space-between'>
@@ -155,6 +191,56 @@ const Homepage = () => {
                                     <Image src='/img/google-play.png' width={132} height={40}/>
                                 </a>
                             </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+
+            <Box position='relative'>
+                <Box position='relative' className={classes.mobile}>
+                    <Hidden smDown>
+                        <ShapeDivider top height={121}>
+                            <svg viewBox="0 0 1500 122" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio='none'>
+                                <path
+                                    d="M225.342 59.4209C501.931 78.0835 724.02 181.41 1069.42 72.6214C1237.09 19.8099 1362.93 3.60655 1448.1 0.965402L1500 1L1500 2.86892e-06L2.12783e-05 0L2.12783e-05 56C40.4754 52.1185 109.194 51.5839 225.342 59.4209Z"/>
+                            </svg>
+                        </ShapeDivider>
+                    </Hidden>
+                </Box>
+                <Box height={190}>
+
+                </Box>
+
+                <Box position="absolute" width='100%' top={156} display="flex" justifyContent="center">
+                    <Box mr={3}>
+                        <Image src='/img/iphone-1.png' width={283} height={566}/>
+                    </Box>
+                    <Image src='/img/iphone-2.png' width={283} height={566}/>
+                </Box>
+            </Box>
+
+            <Box pt={12} pb={15}>
+                <Container>
+                    <Grid container justify='center'>
+                        <Grid item xs={12} sm={6} md={4} className={classes.smSecondMdFirst}>
+                            <Box mb={{xs: 2, md: 4}} textAlign='center'>
+                                <SectionHeader dashAlign='center'>
+                                    Subscribe our newsletter
+                                </SectionHeader>
+                            </Box>
+
+                            <Box mb={{xs: 2, md: 4}} textAlign='center'>
+                                <Typography variant="body1">
+                                    Subscribe to our newsletter for daily/weekly update of our products and services.
+                                </Typography>
+                            </Box>
+
+                            <Box textAlign={{xs: 'center', md: 'right'}}>
+                                <Button>
+                                    Discover
+                                </Button>
+                            </Box>
+
                         </Grid>
                     </Grid>
                 </Container>
